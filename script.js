24d978cef11cb0e2216a65db1dfb2f77609c5138
Luizferrp <luizreis.p@icloud.com>
2024-09-27 22:10:02 -0300

    template

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 window.addEventListener('scroll', () => {
   const message = document.getElementById('message');
-  if (window.scrollY > 0) {
-    message.textContent = 'Mundo';
-  } else {
+  const posts = document.getElementById('posts');
+  if (window.scrollY < 48) {
     message.textContent = 'Olá';
+  } 
+  if (window.scrollY > 48 && window.scrollY < 144){
+    message.textContent = 'Mundo';
+  }
+  if (window.scrollY > 144) {
+    console.log("puts")
+    posts.style.visibility = 'visible';
+    message.remove();
   }
 });
 
